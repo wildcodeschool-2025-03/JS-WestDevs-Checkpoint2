5aa9da5d88f878cc15a083e1cd164ef89a3f229e
tomabadie <abadie.thomas@gmail.com>
2025-05-19 13:15:36 +0200

    Add last details

--- client/src/pages/CupcakeDetails.tsx
+++ client/src/pages/CupcakeDetails.tsx
@@ -15,7 +15,19 @@ const CupcakeDetails = () => {
       });
   }, [id]);
 
-  return <>{cupcake && <Cupcake data={cupcake} />}</>;
+  return (
+    <>
+      {cupcake && (
+        <>
+          <Cupcake data={cupcake} />
+          <p>Name : {cupcake.name}</p>
+          <p>Color 1 : {cupcake.color1}</p>
+          <p>Color 2 : {cupcake.color2}</p>
+          <p>Color 3 : {cupcake.color3}</p>
+        </>
+      )}
+    </>
+  );
 };
 
 export default CupcakeDetails;
